23272b92a217b2262bf624678ba608f98cbef5ac
PudgyPigeon <tommynam1994@gmail.com>
2026-02-02 13:17:03 +0000

    build: Add globalPkgs to home

--- flake.nix
+++ flake.nix
@@ -6,26 +6,19 @@
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     home-manager.url = "github:nix-community/home-manager";
     helix.url = "github:helix-editor/helix/master";
-
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
     let
-      # Static values used across the flake
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
       systemMap = import ./hosts/system-map.nix;
-
-      helixPkg = inputs.helix.packages.${system}.default;
-
-      wslModule = inputs.nixos-wsl.nixosModules.default;
-
-      # Takes the username and the 'kind' of system (e.g., wsl)
       mkHost = username: kind: (systemMap { inherit inputs system username; }).${kind};
     in {  
       formatter.${system} = pkgs.nixpkgs-fmt;
-
+      
       nixosConfigurations = {
         wsl = mkHost "nixos" "wsl";
       };
--- home/nixos-wsl.nix
+++ home/nixos-wsl.nix
@@ -1,36 +1,42 @@
 { config, pkgs, inputs, username, ... }:
 {
-  home-manager.users.${username} = {
-    home.stateVersion = "24.11";
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = { inherit inputs username; };
 
-    # 1. User-specific pacakges
-    home.packages = with pkgs; [
-      git 
-      neovim 
-      wget
-      inputs.helix.packages.${pkgs.system}.helix
-    ];
+    users.${username} = {
+      home.stateVersion = "24.11";
 
-    # 2. SSH Agent and Github keys
-    services.ssh-agent.enable = true;
-    programs.ssh = {
-      enable = true;
-      extraConfig = ''
-        Host github.com
-          IdentityFile ~/.ssh/github_id_ssh_key
-          AddKeysToAgent yes
-      '';
-    };
+      # SSH Agent and Github keys
+      services.ssh-agent.enable = true;
+      programs.ssh = {
+        enable = true;
+        extraConfig = ''
+          Host github.com
+            IdentityFile ~/.ssh/github_id_ssh_key
+            AddKeysToAgent yes
+        '';
+      };
 
-    # 3. Direnv with the Caching "Fix"
-    programs.direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-    };
+      # Direnv and Bash
+      programs.bash.enable = true;
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+      };
 
-    # 4. User-specific Shell Env
-    home.sessionVariables = {
-      EDITOR = "hx";
+      home.packages = with pkgs; [
+        git 
+        neovim 
+        wget
+        inputs.helix.packages.${pkgs.system}.helix
+      ];
+
+      # User-specific Shell Env
+      home.sessionVariables = {
+        EDITOR = "hx";
+      };
     };
   };
 }
